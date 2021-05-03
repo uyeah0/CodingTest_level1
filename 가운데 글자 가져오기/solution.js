d24f66ad7eb2b721b1e700dbc34f27999a91df0c
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-03 21:18:41 +0900

    Update solution.js

--- 가운데 글자 가져오기/solution.js
+++ 가운데 글자 가져오기/solution.js
@@ -2,12 +2,8 @@
 단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
 */
 function solution(s) {
-    var answer = s;
+    var answer = '';
     
-    if((answer.length)%2==0){       
-        answer = answer.substr((answer.length)/2-1,2);
-    }else{
-        answer = answer.substr((answer.length)/2,1);     
-    }
+    answer = (s.length)%2 ? s.substr((s.length)/2,1) : s.substr((s.length)/2-1,2);
     return answer;
 }
