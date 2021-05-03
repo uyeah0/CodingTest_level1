8669469d29a0bfd64b108d841323297f7c10f102
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-03 20:54:57 +0900

    Rename EvenAndOdd.c to 짝수와홀수/EvenAndOdd.c

---
+++ 짝수와홀수/EvenAndOdd.c
@@ -0,0 +1,24 @@
+/*
+문제 설명
+정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+
+제한 조건
+num은 int 범위의 정수입니다.
+0은 짝수입니다.
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(int num) {
+    // 리턴할 값은 메모리를 동적 할당해주세요
+    char* answer = (char*)malloc(sizeof(char) * 4);
+
+    if (num % 2 != 0) {
+        answer = "Odd";
+    }
+    else {
+        answer = "Even";
+    }
+    return answer;
+}}
