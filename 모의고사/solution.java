0f74492cfe610757fbb0d0873f39f87f065fecb1
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-10 23:35:05 +0900

    Add files via upload

---
+++ 모의고사/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        int[] f = new int[answers.length];
+        int[] s = new int[answers.length];
+        int[] t = new int[answers.length];
+        int cnt=1;
+        for(int i = 0; i < answers.length; i++){
+            if(cnt%5==0){
+                f[i] = cnt;
+                cnt =0;
+            }else{
+                f[i] = cnt;    
+            }
+            
+        }
+        for(int i =0; i < anwers.length; i++)      {
+            if(i%2==0){
+                s[i] =cnt;
+                cnt++;
+            }else{
+                s[i] = 2;
+            }
+        }
+        
+        for(int i =0; i <answer.lengthl;i++){
+            //??
+        }
+
+        int fSco, sSco,tSco;
+        for(int i =0; i < answers.length; i ++)        {
+            fSco = 
+        }
+        return answer;
+    }
+}
+void sum(int arr[]){
+    
+}
