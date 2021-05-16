5dce9200947fd4245abdcf310ca742484a23f5dd
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-05-17 04:31:35 +0900

    Update solution.java
    second commit

--- 크레인 인형뽑기 게임/solution.java
+++ 크레인 인형뽑기 게임/solution.java
@@ -3,34 +3,34 @@ class Solution {
         int answer = 0;
         int l = moves.length;
         int cnt=0,n,cntR =0;
-        int result[] = new int[l];
+        int result[] = new int[board[0].length]; // board길이만큼result배열생성
         
         while(cnt < l){
-            n = moves[cnt]-1;
+            
             for(int j = 0; j < board[0].length; j++){
-                if(board[n][j] != 0){
-                    result[cntR] = board[n][j];
-                    board[n][j]=0;
-                    cnt++;
+                n = moves[cnt]-1;
+                if(board[j][n] != 0){
+                    result[cntR] = board[j][n];
+                    board[j][n]=0;
                     cntR++;
                     break;
                 }
             }
-            for(int k =0; k < l-1; k++){
-                for(int y = k+1; y <l; k++){
+            for(int k =0; k < result.length-1; k++){  // 정답 배열 비교 터트리기 
+                for(int y = k+1; y <result.length; k++){
                     if(result[k]==result[y] && result[k]!=0){
                         result[k]=0;
                         result[y]=0;
                         cntR = cntR-2;
+                        answer ++;
+                        break;
                     }
                 }
             }
+            cnt++;
+            
         }
-        int c=0;
-        for(int i =0; i < result.length; i++){
-            if(result[i]!=0)c++;
-        }
-        
+    
         return answer;
     }
 }
