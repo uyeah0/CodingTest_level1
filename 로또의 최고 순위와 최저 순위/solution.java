42fe73d2d821910f786bf842000d3030653625dc
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-06-03 23:57:00 +0900

    Add files via upload

---
+++ 로또의 최고 순위와 최저 순위/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = {};
+        int cnt =0;
+        for(int i =0; i < lottos.length; i++){
+            if(lottos[i]==win_nums[i]) cnt++;
+            else if(lottos[i] ==0) {
+                
+            }
+        }
+        return answer;
+    }
+}
