47213063a6f709bba10a0d40edf86d4a729055f2
uyeah0 <79914479+uyeah0@users.noreply.github.com>
2021-06-04 23:30:51 +0900

    Update solution.java

--- 로또의 최고 순위와 최저 순위/solution.java
+++ 로또의 최고 순위와 최저 순위/solution.java
@@ -1,13 +1,20 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = {};
-        int cnt =0;
-        for(int i =0; i < lottos.length; i++){
-            if(lottos[i]==win_nums[i]) cnt++;
-            else if(lottos[i] ==0) {
-                
+        int[] answer = new int[2];
+        int len = lottos.length;
+        int cntMin =0, cntMax = 0;
+        for(int i =0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                if(lottos[i] == win_nums[j]) {
+                    cntMin++;
+                    cntMax++;
+                 }
             }
+            if(lottos[i] == 0) cntMax++;
         }
+        answer[0] = 7-cntMax;
+        answer[1] = 7-cntMin;
         return answer;
     }
-}
+}        
+       
